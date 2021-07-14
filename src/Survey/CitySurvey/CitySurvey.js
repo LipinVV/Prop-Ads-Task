f410241ad4408e61cd1484eb6d533edb0e9779e6
LipinVV <vit.lipin@gmail.com>
2021-07-15 00:56:03 +0300

    language options fixed mistakes

--- src/Survey/CitySurvey/CitySurvey.js
+++ src/Survey/CitySurvey/CitySurvey.js
@@ -59,7 +59,7 @@ export const CitySurvey = ({chosenLanguage}) => {
     })
     return (
         <div className='city-survey'>
-            <h1 className='city-survey__question'>{chosenLanguage === 'ru' ? word.pickTheCity.slice(0, 14) : word.pickTheCity.slice(0, 9)}<br className='city-survey__question-breaker'/>{chosenLanguage==='ru' ? word.pickTheCity.slice(14, 26) : word.pickTheCity.slice(10, 26)}</h1>
+            <h1 className='city-survey__question'>{chosenLanguage === 'ru' ? word.pickTheCity.slice(0, 14) : word.pickTheCity.slice(0, 10)}<br className='city-survey__question-breaker'/>{chosenLanguage==='ru' ? word.pickTheCity.slice(14, 26) : word.pickTheCity.slice(10, 26)}</h1>
             {!modal && <select className='city-survey__options' value={cityOption}
                      onChange={citySetHandler}>
                 {Object.values(cities).map((city, index) => (
